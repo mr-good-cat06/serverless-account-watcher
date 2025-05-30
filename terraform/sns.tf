a4d3875d01d86c6cbceeaec4fb81b4cc1eb104c7
mr-good-cat06 <tanvir8499@gmail.com>
2025-05-30 13:44:52 +0900

    SNS Done

---
+++ terraform/sns.tf
@@ -0,0 +1,15 @@
+resource "aws_sns_topic" "alert-me" {
+    name= "alertmequick"
+}
+
+resource "aws_sns_topic_subscription" "alert-me-email" {
+    topic_arn = aws_sns_topic.alert-me.arn
+    protocol = "email"
+    endpoint = "${var.email}"
+}
+
+output "sns_topic_arn" {
+  value = aws_sns_topic.alert-me.arn
+  
+}
+
