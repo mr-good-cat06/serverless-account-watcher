1178596eee51c80d97cd958ee9f903576a2d5b7d
mr-good-cat06 <tanvir8499@gmail.com>
2025-06-01 21:09:33 +0900

    complete

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "email" {
     type = string
-    default = "rahmantanvir1407@gmail.com"
+    default = "hfytf@example.com"
 }
 
 variable "slack_webhook_url" {
     type = string
-    default = "https://hooks.slack.com/services/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"
+    default = "ugugu"
 }
 
 
